76f887c449292a7d6231ce0d80a0374808bebbb4
Pulakendu Ghosh <pulak.ghosh90@gmail.com>
2019-10-01 17:10:06 +0530

    overall style fix

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -5,18 +5,15 @@ import { ClassNames } from '@emotion/core';
 
 const StyledButton = styled('button')`
     display: inline-block;
-    margin-bottom: 0;
     font-weight: 400;
     text-align: center;
     white-space: nowrap;
-    vertical-align: middle;
     touch-action: manipulation;
     cursor: pointer;
     background-image: none;
     border: 1px solid transparent;
     padding: 6px 9px;
     font-size: 14px;
-    line-height: 1.42857143;
     border-radius: 4px;
     user-select: none;
     margin: 0 5px;
